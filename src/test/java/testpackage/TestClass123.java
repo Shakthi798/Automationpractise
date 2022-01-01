f69e0d4a8db513123b847f1204d025d1e642e6d1
shakthi798 <shakthi798@gmail.com>
2021-12-31 23:13:48 -0800

    changed to 123

---
+++ src/test/java/testpackage/TestClass123.java
@@ -0,0 +1,48 @@
+package testpackage;
+
+import java.util.Random;
+import org.testng.Assert;
+import org.testng.annotations.Test;
+import objectpackage.DressesPage;
+import objectpackage.ObjectClass;
+
+public class TestClass {
+	ObjectClass submit;
+	ObjectClass select;
+	DressesPage Dp;
+	int random = new Random().nextInt(5001);
+	int random1 = new Random().nextInt(5005);
+
+
+	public TestClass() {
+		submit = new ObjectClass();
+		select = new ObjectClass();
+
+	}
+
+	@Test
+	public void verifyCatagoryTabs() {
+		Assert.assertTrue(select.tabDress().isDisplayed());
+		Assert.assertTrue(select.tabTshirts().isDisplayed());
+		Assert.assertTrue(select.tabWomen().isDisplayed());
+
+	}
+
+	@Test
+	public void verifyNavigatedCorespondingtab() {
+		select.ClickDressmenu();
+		Assert.assertTrue(select.titleblockDresses().isDisplayed());
+		select.ClickTshirtsmenu();
+		Assert.assertTrue(select.titleblockTshirt().isDisplayed());
+		select.ClickDressmenu();
+		Assert.assertTrue(select.titleblockWomen().isDisplayed());
+	}
+
+	@Test
+	public void verifyNewsLetterSuccessfulregisterdAlert() {
+		submit.SubmittoNewsLetter("shakthi" + random + "@gmail.com");
+		String Subscription = submit.newsletterAlert();
+		Assert.assertTrue(Subscription.contains("successfully"));
+	}
+
+}
